0c763f4c4b2f5a68778c59a4b572d587466a1423
ted medeiros <ted.medeiros@ctis.com.br>
2018-07-03 16:02:24 -0300

    Estudo do Jasmine com NodeJs

---
+++ jasmine-node/spec/exemplo/exemploSpec.js
@@ -0,0 +1,7 @@
+describe("Suite de testes de exemplo do Jasmine com Node.js", function(){
+
+    it("Deve demonstrar o uso do Jasmine com o Node.js", function(){
+
+        expect(true).toEqual(true);
+    });
+});
